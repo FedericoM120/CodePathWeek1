fd2b3b789d254e1120b046f64dda8d2fd79aff21
FedericoM120 <monteverdi.102@gmail.com>
2022-09-17 15:54:20 -0400

    added imageview that makes answer choices disappear and added texture and aligned text to gravity center

--- app/src/main/java/com/example/codepathweek1project/MainActivity.kt
+++ app/src/main/java/com/example/codepathweek1project/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.codepathweek1project
 
+import android.graphics.Color
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +24,39 @@ class MainActivity : AppCompatActivity() {
             findViewById<TextView>(R.id.flashcard_question).visibility = View.VISIBLE;
             findViewById<TextView>(R.id.flashcard_answer).visibility = View.INVISIBLE;
         }
+
+        findViewById<TextView>(R.id.choiceOne).setOnClickListener {
+            findViewById<TextView>(R.id.choiceOne).setBackgroundColor(Color.RED);
+        }
+
+        findViewById<TextView>(R.id.choiceTwo).setOnClickListener {
+            findViewById<TextView>(R.id.choiceTwo).setBackgroundColor(Color.RED)
+        }
+
+        findViewById<TextView>(R.id.choiceThree).setOnClickListener {
+            findViewById<TextView>(R.id.choiceThree).setBackgroundColor(Color.GREEN)
+        }
+
+
+        findViewById<ImageView>(R.id.toggle_choices_visibility).setOnClickListener() {
+            findViewById<ImageView>(R.id.toggle_choices_visibility).setImageResource(R.drawable.eye_closed)
+            findViewById<TextView>(R.id.choiceOne).visibility = View.INVISIBLE;
+            findViewById<TextView>(R.id.choiceTwo).visibility = View.INVISIBLE;
+            findViewById<TextView>(R.id.choiceThree).visibility = View.INVISIBLE;
+        }
+
+        fun main() {
+            var variable : Boolean ?= null;
+            if (variable == true) {
+                findViewById<ImageView>(R.id.toggle_choices_visibility).setImageResource(R.drawable.eye_closed)
+                findViewById<TextView>(R.id.choiceOne).visibility = View.INVISIBLE;
+                findViewById<TextView>(R.id.choiceTwo).visibility = View.INVISIBLE;
+                findViewById<TextView>(R.id.choiceThree).visibility = View.INVISIBLE;
+            }
+        }
+
+
+
 
     }
 }
