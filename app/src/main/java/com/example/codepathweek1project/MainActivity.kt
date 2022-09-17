6514523cc02917c11230c330ba2f3481f0aa08e2
FedericoM120 <monteverdi.102@gmail.com>
2022-09-17 11:13:43 -0400

    Added two Textview that shows a question, one that shows question the other shows answer.
    Added listener that makes question disappear and answer appear

---
+++ app/src/main/java/com/example/codepathweek1project/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.codepathweek1project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.flashcard_question).setOnClickListener {
+            findViewById<TextView>(R.id.flashcard_question).visibility = View.INVISIBLE;
+            findViewById<TextView>(R.id.flashcard_answer).visibility = View.VISIBLE;
+        }
+    }
+}
