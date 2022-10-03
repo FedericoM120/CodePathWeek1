567d8eb969461394a37a953e5edc4978a2c434a9
FedericoM120 <monteverdi.102@gmail.com>
2022-10-03 10:16:34 -0400

    too many changes

--- app/src/main/java/com/example/codepathweek1project/MainActivity.kt
+++ app/src/main/java/com/example/codepathweek1project/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.codepathweek1project
 
+import android.content.Intent
 import android.graphics.Color
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
@@ -8,12 +9,52 @@ import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContract
+import com.google.android.material.snackbar.Snackbar
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.myButton).setOnClickListener {
+            val intent = Intent(this, EndingActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+
+        flashcardQuestion.setOnClickListener {
+            flashcardAnswer.visibility = View.VISIBLE
+            flashcardQuestion.visibility = View.INVISIBLE
+        }
+
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+
+            // This code is executed in StartingActivity after we come back from EndingActivity
+
+            // This extracts any data that was passed back from EndingActivity
+            val data: Intent? = result.data
+            // ToDo: Execute more code here
+
+        }
+
+        findViewById<View>(R.id.myButton).setOnClickListener {
+            val intent = Intent(this, EndingActivity::class.java)
+            // Launch EndingActivity with the resultLauncher so we can execute more code
+            // once we come back here from EndingActivity
+            resultLauncher.launch(intent)
+        }
+
+        findViewById<View>(R.id.button).setOnClickListener {
+            finish()
+        }
+
+
+
 
         findViewById<TextView>(R.id.flashcard_question).setOnClickListener {
             findViewById<TextView>(R.id.flashcard_question).visibility = View.INVISIBLE;
@@ -37,7 +78,6 @@ class MainActivity : AppCompatActivity() {
             findViewById<TextView>(R.id.choiceThree).setBackgroundColor(Color.GREEN)
         }
 
-        val isShowingAnswer: Boolean = true;
 
         findViewById<ImageView>(R.id.answers_showing).setOnClickListener() {
             findViewById<ImageView>(R.id.answers_showing).visibility = View.INVISIBLE
@@ -54,6 +94,8 @@ class MainActivity : AppCompatActivity() {
             findViewById<TextView>(R.id.choiceTwo).visibility = View.VISIBLE;
             findViewById<TextView>(R.id.choiceThree).visibility = View.VISIBLE;
         }
+
+
 
     }
 }
